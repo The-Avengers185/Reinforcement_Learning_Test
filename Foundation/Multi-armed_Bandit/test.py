45f01fc340883f12d48149ffb7b1ca1ce16a40d1
ZhuXiangYu <1123640073@qq.com>
2025-04-28 19:08:15 +0800

    Solver类

--- Foundation/Multi-armed_Bandit/test.py
+++ Foundation/Multi-armed_Bandit/test.py
@@ -16,9 +16,35 @@ class BernoulliBandit:
             return 1
         else:
             return 0
-        
+
+class Solver:
+    """多臂老虎机算法基本框架 """
+    def __init__(self, bandit):
+        self.bandit = bandit  # 多臂老虎机实例
+        self.counts = np.zeros(self.bandit.K)  # 每根拉杆被拉动的次数
+        self.regret = 0.0  # 累计遗憾值
+        self.actions = []  # 记录每次选择的拉杆
+        self.regrets = [] 
+
+    def update_regret(self, k):
+        # 计算累积懊悔并保存,k为本次动作选择的拉杆的编号
+        self.regret += self.bandit.best_prob - self.bandit.probs[k]
+        self.regrets.append(self.regret)
+
+    def run_one_step(self):
+        # 返回当前动作选择哪一根拉杆,由每个具体的策略实现
+        raise NotImplementedError
+    
+    def run(self, num_steps):
+        # 运行num_steps步,每一步都选择一根拉杆并更新懊悔值
+        for _ in range(num_steps):
+            k = self.run_one_step() # 选择一根拉杆
+            self.counts[k] += 1
+            self.actions.append(k)
+            self.update_regret(k)
+
 np.random.seed(1) # 设置随机种子,使实验可重复
-K = 10
-bandit_10_arm = BernoulliBandit(K) # 创建一个10臂老虎机实例
-print("随机生成了一个%d臂伯努利老虎机" % K)
-print("获奖概率最大的拉杆为%d号,其获奖概率为%.4f" %(bandit_10_arm.best_idx, bandit_10_arm.best_prob))
+# K = 10
+# bandit_10_arm = BernoulliBandit(K) # 创建一个10臂老虎机实例
+# print("随机生成了一个%d臂伯努利老虎机" % K)
+# print("获奖概率最大的拉杆为%d号,其获奖概率为%.4f" %(bandit_10_arm.best_idx, bandit_10_arm.best_prob))
