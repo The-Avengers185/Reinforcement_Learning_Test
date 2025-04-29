50be7d849438c38e2d9077c5e3ff0ddcfa100148
ZhuXiangYu <1123640073@qq.com>
2025-04-29 19:08:23 +0800

    epsilon-greedy

--- Foundation/Multi-armed_Bandit/test.py
+++ Foundation/Multi-armed_Bandit/test.py
@@ -43,8 +43,53 @@ class Solver:
             self.actions.append(k)
             self.update_regret(k)
 
+class EpsilonGreedy(Solver):
+    """epsilon贪婪算法,继承Solver类 """
+    def __init__(self, bandit, epsilon=0.01, init_prob=0.5):
+        super(EpsilonGreedy, self).__init__(bandit)
+        self.epsilon = epsilon
+        #初始化拉动所有拉杆的期望奖励估值
+        self.estimates = np.array([init_prob] * self.bandit.K)
+
+    def run_one_step(self):
+        if np.random.random() < self.epsilon:
+            k = np.random.randint(0, self.bandit.K) # 随机选择一根拉杆
+        else:
+            k = np.argmax(self.estimates) # 选择期望奖励估值最大的拉杆
+        r = self.bandit.step(k) # 拉动k号拉杆,获得奖励r
+        self.estimates[k] += 1. / (self.counts[k] + 1) * (r - self.estimates[k]) # 更新k号拉杆的期望奖励估值
+        return k
+
+def plot_results(solvers, solver_names):
+    """生成累积懊悔随时间变化的图像。输入solvers是一个列表,列表中的每个元素是一种特定的策略。
+    而solver_names也是一个列表,存储每个策略的名称"""
+    for idx, solver in enumerate(solvers):
+        time_list = range(len(solver.regrets))
+        plt.plot(time_list, solver.regrets, label=solver_names[idx])
+    plt.xlabel('Time steps')
+    plt.ylabel('Cumulative regrets')
+    plt.title('%d-armed bandit' % solvers[0].bandit.K)
+    plt.legend()
+    plt.show()
+
+
 np.random.seed(1) # 设置随机种子,使实验可重复
-# K = 10
-# bandit_10_arm = BernoulliBandit(K) # 创建一个10臂老虎机实例
+K = 10
+bandit_10_arm = BernoulliBandit(K) # 创建一个10臂老虎机实例
 # print("随机生成了一个%d臂伯努利老虎机" % K)
 # print("获奖概率最大的拉杆为%d号,其获奖概率为%.4f" %(bandit_10_arm.best_idx, bandit_10_arm.best_prob))
+epsilon_greedy_solver = EpsilonGreedy(bandit_10_arm, epsilon=0.01)
+epsilon_greedy_solver.run(5000)
+print('epsilon-贪婪算法的累积懊悔为：', epsilon_greedy_solver.regret)
+plot_results([epsilon_greedy_solver], ["EpsilonGreedy"])
+
+np.random.seed(0)
+epsilons = [1e-4, 0.01, 0.1, 0.25, 0.5]
+epsilon_greedy_solver_list = [
+    EpsilonGreedy(bandit_10_arm, epsilon=e) for e in epsilons
+]
+epsilon_greedy_solver_names = ["epsilon={}".format(e) for e in epsilons]
+for solver in epsilon_greedy_solver_list:
+    solver.run(5000)
+
+plot_results(epsilon_greedy_solver_list, epsilon_greedy_solver_names)
